58dfa39c4b5c51bd261ec25e42137ff55bc5e975
Christian Fei <crifei93@gmail.com>
2018-05-22 23:56:11 +0200

    add test for scrape

---
+++ test/scrape.js
@@ -0,0 +1,21 @@
+/* globals test */
+
+const {deepEqual} = require('assert')
+
+const scrape = require('../lib/scrape')
+
+test('scrapes bitcoin historic data for May 1st, 2018', (done) => {
+  scrape('bitcoin', '20180501', '20180501').then(historic => {
+    console.log(JSON.stringify(historic, null, 2))
+    deepEqual(historic, [{
+      'time': 1525125600000,
+      'open': 9251.47,
+      'high': 9255.88,
+      'low': 8891.05,
+      'close': 9119.01,
+      'volume': '7,713,020,000'
+    }])
+
+    done()
+  })
+})
