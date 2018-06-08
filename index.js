91a607b5932d53eaf11a466064d775d3c93fe140
Christian Fei <crifei93@gmail.com>
2018-06-08 17:09:48 +0200

    remove obsolete console.log

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ async function main (coin, start, end) {
   coin = coin || 'bitcoin'
   start = start || '20180501'
   end = end || '20180531'
+
   const historic = await scrape(coin, start, end)
 
-  console.log(JSON.stringify(historic, null, 2))
   writeFileSync(join(__dirname, 'samples', `${coin}-${start}-${end}.json`), JSON.stringify(historic, null, 2))
 }
